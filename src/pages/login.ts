01b9515ce5c238fa31a8536e309c07f0d320e264
purna99 <cpurna1@gmail.com>
2017-10-04 19:50:16 -0500

    updated login component

--- src/pages/login.ts
+++ src/pages/login.ts
@@ -26,4 +26,40 @@ export class LoginPage {
       this.email.setFocus();
     }, 500);
   }
+
+  login(): void {
+    this.oauthService.createAndSaveNonce().then(nonce => {
+      const authClient = new OktaAuth({
+        clientId: this.oauthService.clientId,
+        redirectUri: this.oauthService.redirectUri,
+        url: this.oauthService.issuer
+      });
+      authClient.signIn({
+        username: this.username,
+        password: this.password
+      }).then((response) => {
+        if (response.status === 'SUCCESS') {
+          authClient.token.getWithoutPrompt({
+            nonce: nonce,
+            responseType: ['id_token', 'token'],
+            sessionToken: response.sessionToken,
+            scopes: this.oauthService.scope.split(' ')
+          })
+            .then((tokens) => {
+              // oauthService.processIdToken doesn't set an access token
+              // set it manually so oauthService.authorizationHeader() works
+              localStorage.setItem('access_token', tokens[1].accessToken);
+              this.oauthService.processIdToken(tokens[0].idToken, tokens[1].accessToken);
+              this.navCtrl.push(TabsPage);
+            })
+            .catch(error => console.error(error));
+        } else {
+          throw new Error('We cannot handle the ' + response.status + ' status');
+        }
+      }).fail((error) => {
+        console.error(error);
+        this.error = error.message;
+      });
+    });
+  }
 }
